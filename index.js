02d685b57ef701a3504514bc050088e0671b6be8
Ashley Levi <Ashplevi@gmail.com>
2018-08-25 13:42:05 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// variables
+var websiteTitle = document.querySelector('.website-title');
+var websiteUrl = document.querySelector('.website-url');
+var enterButton = document.querySelector('.enter-button', createBookmark);
+
+console.log(websiteTitle);
+console.log(websiteUrl);
+console.log(enterButton);
+
+// event listeners
+// websiteTitle.addEventListener('keyup', )
+// websiteUrl.addEventListener('keyup', )
+// enterButton.addEventListener('click', )
+
+// functions
+function createBookmark() {
+title.value;
+url.value;
+}
