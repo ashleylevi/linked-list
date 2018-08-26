07edf498771ebcad66f51d34b5c84fb2d10b8503
Ashley Levi <Ashplevi@gmail.com>
2018-08-26 17:48:36 -0600

    Added delete and read function

--- index.js
+++ index.js
@@ -1,28 +1,73 @@
 // variables
 var websiteTitle = document.querySelector('.website-title');
 var websiteUrl = document.querySelector('.website-url');
-var enterButton = document.querySelector('.enter-button', createBookmark);
+var enterButton = document.querySelector('.enter-button');
+// var readButton = document.querySelector('.unread');
+// var readButton = document.querySelector('.unread');
+//   var deleteButton = document.querySelector('.delete');
 
-console.log(websiteTitle);
-console.log(websiteUrl);
-console.log(enterButton);
 
 // event listeners
-// websiteTitle.addEventListener('keyup', )
-// websiteUrl.addEventListener('keyup', )
-enterButton.addEventListener('click', createBookmark)
+// websiteTitle.addEventListener('keyup', enableSubmitButton);
+// websiteUrl.addEventListener('keyup', enableSubmitButton);
+enterButton.addEventListener('click', createBookmark);
+// readButton.addEventListener('click', markAsRead);
+
+
 
 // functions
 function createBookmark(event) {
   event.preventDefault();
   var bookmark = document.createElement('div');
-  bookmark.innerHTML = `<h1 >${websiteTitle.value}</h1> 
-                        <hr />
-                        <h2><a href="${websiteUrl.value}">${websiteUrl.value}</a></h2>
-                        <hr />
-                        <button class="read">Read</button>
-                        <button class="delete">Delete</button>`
-  document.getElementById('output-section').appendChild(bookmark);  
+  bookmark.innerHTML = 
+  `<h1>${websiteTitle.value}</h1> 
+  <hr />
+  <h2><a href="https://${websiteUrl.value}" target="_blank">${websiteUrl.value}</a></h2>
+  <hr />
+  <button class='unread'>Read</button>
+  <button class="delete">Delete</button>`
+  document.getElementById('output-section').prepend(bookmark); 
+  
+  var readButton = document.querySelector('.unread');
+  var deleteButton = document.querySelector('.delete');
+  
+  readButton.addEventListener('click', markAsRead); 
+  deleteButton.addEventListener('click', deleteBookmark);
+
+  function markAsRead() {
+    if (readButton.className === 'read') {
+      readButton.className = 'unread';
+    } else {
+      readButton.className = 'read';
+    } 
 }
 
-console.log(createBookmark);
+  function deleteBookmark(event) {
+    if (event.target.className === 'delete') {
+    event.target.parentNode.remove();
+  }
+
+}
+}
+
+function enableSubmitButton() {
+  if (websiteTitle.value.length === 0 || websiteUrl.value.length === 0) {
+    enterButton.disabled = true;
+  } else {
+    enterButton.disabled = false;
+  }
+}
+
+
+
+// function errorMessage() {
+//   if (websiteTitle.value === '' && websiteUrl.value === '') {
+//     var errorMessage = document.createElement('p');
+//     errorMessage.innerHTML = '<p>Please enter a website title and a valid URL!</p>'
+//     document.querySelector('.error-message').prepend(errorMessage);
+
+//   }
+// }
+
+
+
