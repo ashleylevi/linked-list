c0989989e21e49e210f138ac135fcf912064ac27
Ashley Levi <Ashplevi@gmail.com>
2018-09-26 10:53:48 -0600

    Used a linter to clean up the code

--- index.js
+++ index.js
@@ -18,44 +18,43 @@ outputSection.addEventListener('click', markAsRead);
 
 // functions
 function createBookmark(event) {
-   event.preventDefault();
-   var bookmark = document.createElement('div');
-   bookmark.innerHTML =
-   `<h1>${websiteTitle.value}</h1>
-   <hr />
-   <h2><a href="${websiteUrl.value}" target="_blank">${websiteUrl.value}</a></h2>
-   <hr />
-   <button aria-label="read" class='unread'>Read</button>
-   <button aria-label="delete" class="delete">Delete</button>`
-   document.getElementById('output-section').prepend(bookmark);
-   var readButton = document.querySelector('.unread');
-   var deleteButton = document.querySelector('.delete');
-   deleteButton.addEventListener('click', deleteBookmark);
-   bookmarkCount++;
-   bookmarkNumber.innerText = bookmarkCount;
-   clearInputs();
-};
+  event.preventDefault();
+  var bookmark = document.createElement('div');
+  bookmark.innerHTML =
+  `<h1>${websiteTitle.value}</h1>
+  <hr />
+  <h2><a href="${websiteUrl.value}" target="_blank">${websiteUrl.value}</a></h2>
+  <hr />
+  <button aria-label="read" class='unread'>Read</button>
+  <button aria-label="delete" class="delete">Delete</button>`
+  document.getElementById('output-section').prepend(bookmark);
+  var readButton = document.querySelector('.unread');
+  var deleteButton = document.querySelector('.delete');
+  deleteButton.addEventListener('click', deleteBookmark);
+  bookmarkCount++;
+  bookmarkNumber.innerText = bookmarkCount;
+  clearInputs();
+}
 
 function markAsRead(event) {
   if (event.target.className === 'read') {
-   event.target.className = 'unread';
-   readCount--;
+    event.target.className = 'unread';
+    readCount--;
+  } else if (event.target.className === 'unread') {
+    event.target.className = 'read';
+    readCount++;
   } 
-   else if (event.target.className === 'unread') {
-     event.target.className = 'read';
-     readCount++;
-   } 
-   event.target.parentNode.classList.toggle('read-bookmark')
-   readNumber.innerText = readCount;
-};
+  event.target.parentNode.classList.toggle('read-bookmark')
+  readNumber.innerText = readCount;
+}
 
 function enableSubmitButton() {
   if (websiteTitle.value.length === 0 || websiteUrl.value.length === 0) {
     enterButton.disabled = true;
   } else {
     enterButton.disabled = false;
- }
-};
+  }
+}
 
 function deleteBookmark(event) {
   if (event.target.className === 'delete' && event.target.parentNode.classList.contains('read-bookmark')) {
@@ -66,14 +65,13 @@ function deleteBookmark(event) {
     event.target.parentNode.remove();
     bookmarkCount--;
   } 
-    bookmarkNumber.innerText = bookmarkCount;
-    readNumber.innerText = readCount;
-};
+  bookmarkNumber.innerText = bookmarkCount;
+  readNumber.innerText = readCount;
+}
 
 
 function clearInputs() {
   websiteTitle.value = '';
   websiteUrl.value = '';
   enableSubmitButton()
-
 }
